3bbdbf25f9f8766fa9e58e2113e003306ce5c756
David Eriksson <davideriksson@swedenmail.com>
2018-04-28 21:42:20 +0200

    Add errorhandling and health endpoint to logic

--- sa-logic/sa/sentiment_analysis.py
+++ sa-logic/sa/sentiment_analysis.py
@@ -6,21 +6,22 @@ app = Flask(__name__)
 
 @app.route("/analyse/sentiment", methods=['POST'])
 def analyse_sentiment():
-    print("HEJ")
-    sentence = request.get_json()['sentence']
-    print("SENTENCE: " + sentence)
-    polarity = TextBlob(sentence).sentences[0].polarity
-    print("POLARITY: " + polarity)
-    return jsonify(
-        sentence=sentence,
-        polarity=polarity
-    )
+    try:
+        sentence = request.get_json()['sentence']
+        polarity = TextBlob(sentence).sentences[0].polarity
+        return jsonify(
+            sentence=sentence,
+            polarity=polarity
+        )
+    except Exception as e:
+        return repr(e), 500
 
 
 @app.route("/health", methods=['GET'])
 def health():
-    return jsonify('{"status":"UP"}')
+    return '{"status":"UP"}'
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run(host='localhost', port=5000)
